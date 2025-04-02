a76ed8333cf169be727d19719a8c1d6bde7023da
xzs <xuzhisen0326@163.com>
2025-04-02 18:33:38 +0800

    提交

---
+++ src/main/java/fun/sensen/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package fun.sensen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
